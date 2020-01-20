ba888f08bd5b11473cd52cd98592d3b3ba27ac30
admin <admin@localhost.localdomain>
2020-01-20 16:03:11 +0100

    update

--- Docs/sncf.sql
+++ Docs/sncf.sql
@@ -1,19 +1,20 @@
-CREATE DATABASE IF NOT EXIST sncf;
-CREATE TABLE IF NOT EXIST ville (
+CREATE DATABASE IF NOT EXISTS sncf;
+USE sncf
+CREATE TABLE IF NOT EXISTS ville (
 	code_postal INT PRIMARY KEYS NOT NULL,
 	nom VARCHAR(50)
 );
-CREATE TABLE IF NOT EXIST gare (
+CREATE TABLE IF NOT EXISTS gare (
 	id_gare INT PRIMARY KEYS NOT NULL,
-	nom VARCHAR(50)
-id INT NOT NULL FOREIGN KEY references ville(code_postal)
+	nom VARCHAR(50),
+	id INT NOT NULL FOREIGN KEY references ville(code_postal)
 );
-CREATE TABLE IF NOT EXIST tgv (
+CREATE TABLE IF NOT EXISTS tgv (
 	id INT PRIMARY KEYS NOT NULL,
 	heure_depart DATETIME,
-	heure_arrive DATETIME
-	gare_depart INT NOT NULL FOREIGN KEY references gare(id_gare);
-gare_arrive INT NOT NULL FOREIGN KEY references gare(id_gare)
+	heure_arrive DATETIME,
+	gare_depart INT NOT NULL FOREIGN KEY references gare(id_gare),
+	gare_arrive INT NOT NULL FOREIGN KEY references gare(id_gare)
 );
 CREATE USER IF NOT EXIST ‘user’@’%’ IDENTIFIED BY ‘user’;
 GRANT ALL PRIVILEGES ON *.* TO ‘user’@’%’;
