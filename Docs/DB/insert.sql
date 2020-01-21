c9cb420d3966d33b9cbd7c9ea16941caa9535a08
admin <admin@localhost.localdomain>
2020-01-21 10:20:38 +0100

    new branch

---
+++ Docs/DB/insert.sql
@@ -0,0 +1,4 @@
+LOAD DATA INFILE '/tmp/dataset/tgv.txt' INTO TABLE tgv FIELDS TERMINATED BY ',';
+LOAD DATA INFILE '/tmp/dataset/gare.txt' INTO TABLE gare FIELDS TERMINATED BY ',';
+LOAD DATA INFILE '/tmp/dataset/ville.txt' INTO TABLE ville FIELDS TERMINATED BY ',';
+
