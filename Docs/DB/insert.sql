cc86f201ba3691c9f39d7cff042a860e2c74db4a
admin <admin@localhost.localdomain>
2020-01-21 11:01:18 +0100

    update

--- Docs/DB/insert.sql
+++ Docs/DB/insert.sql
@@ -1,4 +1,7 @@
-LOAD DATA INFILE '/tmp/dataset/tgv.txt' INTO TABLE tgv FIELDS TERMINATED BY ',';
-LOAD DATA INFILE '/tmp/dataset/gare.txt' INTO TABLE gare FIELDS TERMINATED BY ',';
-LOAD DATA INFILE '/tmp/dataset/ville.txt' INTO TABLE ville FIELDS TERMINATED BY ',';
+use sncf
+LOAD DATA LOCAL INFILE '/tmp/dataset/ville.txt' INTO TABLE ville FIELDS TERMINATED BY ',';
+LOAD DATA LOCAL INFILE '/tmp/dataset/gare.txt' INTO TABLE gare FIELDS TERMINATED BY ',';
+LOAD DATA LOCAL INFILE '/tmp/dataset/tgv.txt' INTO TABLE tgv FIELDS TERMINATED BY ',';
+
+
 
