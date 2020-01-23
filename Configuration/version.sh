1fd7349127b994d3340515f0e456057dc354966d
admin <admin@localhost.localdomain>
2020-01-23 10:20:58 +0100

    update script

--- Configuration/version.sh
+++ Configuration/version.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-export VERSION_APPLY='2'
+export VERSION_APPLY=2
+echo $VERSION_APPLY
+/usr/local/bin/docker-compose build
+docker tag configuration_httpdserver eyja1994/server_sncf:$VERSION_APPLY
